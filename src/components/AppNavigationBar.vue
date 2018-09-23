81d0dd7fada1f4f7d4aec1fd4d8140c777eeb8a8
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-24 00:34:22 +0200

    Add a seperate div for the borders for the nav bars
    -- this is to make the borders stretch 100%
       of the screen whilst  maintaining the container
       width of the child elements

--- src/components/AppNavigationBar.vue
+++ src/components/AppNavigationBar.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="flex items-center justify-between container mx-auto border-b">
-    <div class="py-2 inline-flex items-center">
-      <div class="h-8 w-8 bg-grey rounded-full mr-4">&nbsp;</div>
-      <input type="text" class="py-2 px-4 border-2 border-grey-light rounded w-64" placeholder="Search: eg Belvedere, Harare">
-    </div>
+  <div class="border-b bg-white">
+    <div class="flex items-center justify-between container mx-auto">
+      <div class="py-2 inline-flex items-center">
+        <div class="h-8 w-8 bg-grey rounded-full mr-4">&nbsp;</div>
+        <input type="text" class="py-2 px-4 border-2 border-grey-light rounded w-64" placeholder="Search: eg Belvedere, Harare">
+      </div>
 
-    <nav class="py-2">
-      <a href="#" class="no-underline font-medium text-grey-darkest mr-4">Rent Out Your House</a>
-      <a href="#" class="no-underline font-medium text-grey-darkest mr-4">Create Account</a>
-      <a href="#" class="no-underline font-medium text-grey-darkest">Login</a>
-    </nav>
+      <nav class="py-2">
+        <a href="#" class="no-underline font-medium text-grey-darkest mr-4">Rent Out Your House</a>
+        <a href="#" class="no-underline font-medium text-grey-darkest mr-4">Create Account</a>
+        <a href="#" class="no-underline font-medium text-grey-darkest">Login</a>
+      </nav>
+    </div>
   </div>
 </template>
 
--- src/pages/PropertiesListing.vue
+++ src/pages/PropertiesListing.vue
@@ -4,20 +4,22 @@
     <AppNavigationBar/>
 
     <!-- Secondary Navigation Bar, with filters -->
-    <div class="flex items-center justify-between container mx-auto border-b">
-      <div class="py-2">
-        <input type="checkbox" name="" id="">
-        <span>Show Map</span>
-      </div>
+    <div class="border-b">
+      <div class="flex items-center justify-between container mx-auto">
+        <div class="py-2">
+          <input type="checkbox" name="" id="">
+          <span>Show Map</span>
+        </div>
 
-      <div class="inline-flex items-center py-2">
-        <span class="mr-4 inline-flex items-center font-bold">
-          <svg class="h-4 mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12 12l8-8V0H0v4l8 8v8l4-4v-4z"/></svg>
-          Filters
-        </span>
-        <button class="py-2 px-4 bg-white rounded border hover:bg-grey-lighter mr-2">Distance from HIT</button>
-        <button class="py-2 px-4 bg-white rounded border hover:bg-grey-lighter mr-2">Price</button>
-        <button class="py-2 px-4 bg-white rounded border hover:bg-grey-lighter">Number of occupants</button>
+        <div class="inline-flex items-center py-2">
+          <span class="mr-4 inline-flex items-center font-bold">
+            <svg class="h-4 mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12 12l8-8V0H0v4l8 8v8l4-4v-4z"/></svg>
+            Filters
+          </span>
+          <button class="py-2 px-4 bg-white rounded border hover:bg-grey-lighter mr-2">Distance from HIT</button>
+          <button class="py-2 px-4 bg-white rounded border hover:bg-grey-lighter mr-2">Price</button>
+          <button class="py-2 px-4 bg-white rounded border hover:bg-grey-lighter">Number of occupants</button>
+        </div>
       </div>
     </div>
 
