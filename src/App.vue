73ec54dd9168915698f0afa7ee9902742ed23b88
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-24 01:15:36 +0200

    show the background color full page
    -- even on pages where the content < 100vh

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,7 @@ export default {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  /* Fix the background color not filling the whole screen */
+  min-height: 100vh;
 }
 </style>
