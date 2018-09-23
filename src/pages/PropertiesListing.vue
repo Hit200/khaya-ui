2650c35efda12bae028cd3b6df98fb774935caec
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-23 23:00:47 +0200

    Rename Hello World to PropertyListings

--- src/pages/PropertiesListing.vue
+++ src/pages/PropertiesListing.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
+    <AppNavigationBar/>
     <h1>Hit 200 Here we come</h1>
   </div>
 </template>
 
 <script>
+import AppNavigationBar from '@/components/AppNavigationBar'
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  components: {
+    AppNavigationBar
+  }
 }
 </script>
 
