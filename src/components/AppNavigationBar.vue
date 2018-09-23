2d04a89aebe3f058a4021ac2ce8e38f12a21654b
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-23 23:01:15 +0200

    Create a seperate component for the Application Nav Bar

---
+++ src/components/AppNavigationBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex items-center justify-between container mx-auto border-b">
+    <div class="py-2 inline-flex items-center">
+      <div class="h-8 w-8 bg-grey-lightest rounded-full mr-4">&nbsp;</div>
+      <input type="text" class="py-2 px-4 border-2 border-grey-light rounded w-64" placeholder="Search: eg Belvedere, Harare">
+    </div>
+
+    <nav class="py-2">
+      <a href="#" class="no-underline text-grey-darkest mr-4">Rent Out Your House</a>
+      <a href="#" class="no-underline text-grey-darkest mr-4">Create Account</a>
+      <a href="#" class="no-underline text-grey-darkest">Login</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
