a13cacbb9f7c3871d5954b2ab15a34a0874ed5b4
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-24 21:04:08 +0200

    Make the AppNavigationBar responsive
    -- this will make it look good on mobile.
       some features e.g hamburger menu
       are not yet functional

--- src/components/AppNavigationBar.vue
+++ src/components/AppNavigationBar.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="border-b bg-white">
-    <div class="flex items-center justify-between container mx-auto">
-      <div class="py-2 inline-flex items-center">
-        <div class="h-8 w-8 bg-grey rounded-full mr-4">&nbsp;</div>
-        <input type="text" class="py-2 px-4 border-2 border-grey-light rounded w-64" placeholder="Search: eg Belvedere, Harare">
+    <div class="flex items-center justify-between sm:flex-wrap container mx-auto px-4 sm:px-0">
+      <div class="w-full lg:w-auto py-2 inline-flex items-center">
+        <div class="h-8 w-8 bg-grey rounded-full mr-4 flex-no-shrink">&nbsp;</div>
+        <input type="text" class="py-2 px-4 border-2 border-grey-light rounded lg:w-64 flex-1" placeholder="Search: eg Belvedere, Harare">
       </div>
 
-      <nav>
-        <router-link to="/" class="py-4 border-b-2 border-transparent hover:border-purple no-underline font-medium text-grey-darkest px-4">Explore houses</router-link>
-        <a href="#" class="py-4 border-b-2 border-transparent hover:border-purple no-underline font-medium text-grey-darkest px-4">Rent Out Your House</a>
-        <a href="#" class="py-4 border-b-2 border-transparent hover:border-purple no-underline font-medium text-grey-darkest px-4">Create Account</a>
+      <nav class="hidden sm:block p-4 lg:p-0">
+        <router-link to="/" class="py-4 border-b-2 border-transparent hover:border-purple no-underline font-medium text-grey-darkest px-2 lg:px-4">Explore houses</router-link>
+        <a href="#" class="py-4 border-b-2 border-transparent hover:border-purple no-underline font-medium text-grey-darkest px-2 lg:px-4">Rent Out Your House</a>
+        <a href="#" class="py-4 border-b-2 border-transparent hover:border-purple no-underline font-medium text-grey-darkest px-2 lg:px-4">Create Account</a>
         <router-link to="/login" class="no-underline font-medium text-white py-2 px-4 rounded bg-purple">Login</router-link>
+      </nav>
+
+      <!-- The hamburger menu -->
+      <nav class="sm:hidden ml-4">
+        <svg class="h-4" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
       </nav>
     </div>
   </div>
