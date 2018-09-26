ce632b436dc2f8ef15ee87e6d6c7fd7e3f489be1
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2018-09-26 12:49:10 +0200

    Add a CreateNewHouse Page to the project
    -- here the house owners will be able to upload
       information about their houses

---
+++ src/pages/CreateNewHouse.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- Primary Navigation bar -->
+    <AppNavigationBar/>
+
+    <div>
+      We will create your house here
+    </div>
+  </div>
+</template>
+
+<script>
+import AppNavigationBar from '@/components/AppNavigationBar'
+export default {
+  name: 'CrceateNewHouse',
+  components: {
+    AppNavigationBar
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import PropertiesListing from '@/pages/PropertiesListing'
+import CreateNewHouse from '@/pages/CreateNewHouse'
 import Login from '@/pages/Login'
 import SingleProperty from '@/pages/SingleProperty'
 import Checkout from '@/pages/Checkout'
@@ -19,6 +20,11 @@ export default new Router({
       path: '/',
       name: 'PropertiesListing',
       component: PropertiesListing
+    },
+    {
+      path: '/new',
+      name: 'CreateNewHouse',
+      component: CreateNewHouse
     },
     {
       path: '/:id',
