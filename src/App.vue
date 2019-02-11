4915f9e6ba75891b10191084f5228c0595f04381
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2019-02-11 11:43:32 +0200

    Reduce font size

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="text-grey-darkest bg-grey-lighter border-t-2 border-purple">
+  <div id="app" class="text-grey-darkest bg-grey-lighter border-t-2 border-purple text-sm">
     <router-view/>
   </div>
 </template>
