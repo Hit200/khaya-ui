d8b43004cdff46ce02b7cac4e542f75f0000e380
Kudakwashe Paradzayi <kgparadzayi@gmail.com>
2019-02-11 11:49:24 +0200

    Make the property listings more spaced out

--- src/pages/PropertiesListing.vue
+++ src/pages/PropertiesListing.vue
@@ -12,15 +12,31 @@
 
         <div class="inline-flex items-center flex-col md:flex-row py-2 w-full md:w-auto">
           <span class="mb-2 md:mb-0 mr-4 inline-flex items-center font-bold">
-            <svg class="h-4 mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12 12l8-8V0H0v4l8 8v8l4-4v-4z"/></svg>
+            <svg
+              class="h-4 mr-1"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M12 12l8-8V0H0v4l8 8v8l4-4v-4z"></path>
+            </svg>
             Filters
           </span>
-          <button class="flex justify-between border-purple w-4/5 md:w-64 mb-2 md:mb-0 py-2 pt-4 px-4 bg-whitel rounded border hover:bg-grey-lighter md:mr-2 relative">
-            <span class="text-sm  pin-t -mt-2 absolute rounded-full px-4 border-b border-purple bg-grey-lighter">Distance from campus</span>
-            <span class="text-purple">798 meters</span> <span>X</span>
+          <button
+            class="flex justify-between border-purple w-4/5 md:w-64 mb-2 md:mb-0 py-2 pt-4 px-4 bg-whitel rounded border hover:bg-grey-lighter md:mr-2 relative"
+          >
+            <span
+              class="text-sm pin-t -mt-2 absolute rounded-full px-4 border-b border-purple bg-grey-lighter"
+            >Distance from campus</span>
+            <span class="text-purple">798 meters</span>
+            <span>X</span>
           </button>
-          <button class="w-4/5 md:w-64 mb-2 md:mb-0 py-2 px-4 bg-white rounded border hover:bg-grey-lighter md:mr-2">Price</button>
-          <button class="w-4/5 md:w-64 mb-2 md:mb-0 py-2 px-4 bg-white rounded border hover:bg-grey-lighter">Number of occupants</button>
+          <button
+            class="w-4/5 md:w-64 mb-2 md:mb-0 py-2 px-4 bg-white rounded border hover:bg-grey-lighter md:mr-2"
+          >Price</button>
+          <button
+            class="w-4/5 md:w-64 mb-2 md:mb-0 py-2 px-4 bg-white rounded border hover:bg-grey-lighter"
+          >Number of occupants</button>
         </div>
       </div>
     </div>
@@ -28,66 +44,94 @@
     <!-- The properties list -->
     <AppMap :show="isShowingMap"/>
 
-    <div class="flex flex-wrap container mx-auto mt-6">
-      <!-- The Property wrapper -->
-      <div class="w-full sm:w-1/2 md:w-1/3 p-2 mb-4" v-for="i in 10" :key="i">
-        <!-- The Property -->
-        <div class="w-full rounded-t overflow-hidden">
-          <!-- The image for the Property -->
-          <div class="w-full h-32 bg-purple overflow-hidden">
-            <img class="w-full" src="/static/sample-house.jpeg" alt="">
-            <!-- Do a lil morre research on how to make the imimages scale properly - consider using object-fit -->
-          </div>
-
-          <!-- The profile pic and the price -->
-          <div class="flex justify-between -mt-8">
-            <div class="w-16 h-16 flex items-center justify-center bg-grey border-4 border-grey-lighter rounded-full overflow-hidden ml-4">
-              <img class="w-full h-full" src="/static/sample-avatar.png" alt="">
-            </div>
-            <span class="self-end mb-1 font-medium">$80 - $120/month</span>
-          </div>
-          
-          <!-- The description items -->
-          <div class="mt-2">
-            <router-link :to="`/${i}`" class="no-underline text-current-color hover:text-black text-lg mb-2 font-bold">47 Castens, Belveder, Harare</router-link>
-            <div class="mb-2">
-              <span>Shared &middot; 9 rooms</span>
+    <div class="container mx-auto mt-6">
+      <!-- Negativley offset the x margin to compensate for the x padding of children -->
+      <div class="-mx-6 flex flex-wrap">
+        <!-- The Property wrapper -->
+        <div class="w-full sm:w-1/2 md:w-1/3 p-2 px-6 mb-4" v-for="i in 10" :key="i">
+          <!-- The Property -->
+          <div class="w-full rounded-t overflow-hidden">
+            <!-- The image for the Property -->
+            <div class="w-full h-32 bg-purple overflow-hidden">
+              <img class="w-full" src="/static/sample-house.jpeg" alt>
+              <!-- Do a lil morre research on how to make the imimages scale properly - consider using object-fit -->
             </div>
 
-            <div class="flex w-full items-center justify-between mb-2">
-              <span class="inline-flex items-center">
-                <svg class="h-4 mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"/></svg>
-                12k
-              </span>
+            <!-- The profile pic and the price -->
+            <div class="flex justify-between -mt-8">
+              <div
+                class="w-16 h-16 flex items-center justify-center bg-grey border-4 border-grey-lighter rounded-full overflow-hidden ml-4"
+              >
+                <img class="w-full h-full" src="/static/sample-avatar.png" alt>
+              </div>
+              <span class="self-end mb-1 font-medium">$80 - $120/month</span>
+            </div>
 
-              <span class="inline-flex items-center">
-                <svg v-for="i in 4" :key="i" fill="currentColor" class="h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"/></svg>
-              </span>
+            <!-- The description items -->
+            <div class="mt-2">
+              <router-link
+                :to="`/${i}`"
+                class="no-underline text-current-color hover:text-black text-lg mb-2 font-bold"
+              >47 Castens, Belveder, Harare</router-link>
+              <div class="mb-2">
+                <span>Shared &middot; 9 rooms</span>
+              </div>
+
+              <div class="flex w-full items-center justify-between mb-2">
+                <span class="inline-flex items-center">
+                  <svg
+                    class="h-4 mr-2"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.78 7.77L10 18.78l8.39-8.4a5.5 5.5 0 0 0-7.78-7.77l-.61.61z"
+                    ></path>
+                  </svg>
+                  12k
+                </span>
+                
+                <span class="inline-flex items-center">
+                  <svg
+                    v-for="i in 4"
+                    :key="i"
+                    fill="currentColor"
+                    class="h-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z"
+                    ></path>
+                  </svg>
+                </span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-   </div>
+    </div>
   </div>
 </template>
 
 <script>
-import AppNavigationBar from '@/components/AppNavigationBar'
-import AppToggle from '@/components/AppToggle'
-import AppMap from '@/components/AppMap'
+import AppNavigationBar from "@/components/AppNavigationBar";
+import AppToggle from "@/components/AppToggle";
+import AppMap from "@/components/AppMap";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   components: {
     AppNavigationBar,
     AppToggle,
     AppMap
   },
-  data () {
+  data() {
     return {
       isShowingMap: true
-    }
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
